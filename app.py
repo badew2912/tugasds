f912ac8f687049e7efe302aec2f65f4ae330a898
badew2912 <92229838+badew2912@users.noreply.github.com>
2023-06-15 15:35:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+# Tampilkan judul aplikasi
+st.title("Prediksi Kebakaran Hutan dan Lahan")
+
+# Ubah URL dengan URL file dataset yang sesuai di repositori GitHub Anda
+dataset_url = "https://raw.githubusercontent.com/USERNAME/REPO/main/dataset.csv"
+
+# Baca dataset
+data = pd.read_csv(dataset_url)
+
+# Tampilkan dataset
+st.subheader("Data:")
+st.write(data)
+
+# Bagi dataset menjadi fitur dan target
+X = data.drop('target', axis=1)
+y = data['target']
+
+# Bagi dataset menjadi data latih dan data uji
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Inisialisasi model Decision Tree Classifier
+clf = DecisionTreeClassifier()
+
+# Latih model
+clf.fit(X_train, y_train)
+
+# Prediksi data uji
+y_pred = clf.predict(X_test)
+
+# Hitung akurasi model
+accuracy = accuracy_score(y_test, y_pred)
+
+# Tampilkan hasil prediksi dan akurasi
+st.subheader("Hasil Prediksi:")
+st.write(pd.DataFrame({'Actual': y_test, 'Predicted': y_pred}))
+st.subheader("Akurasi:")
+st.write(accuracy)
